bb3579868760bee7d2c84ede7a6b1beee145077f
e-harman <harman.eric@gmail.com>
2017-05-07 16:36:36 -0700

    First working commit

--- caesar/csc_file.py
+++ caesar/csc_file.py
@@ -1,37 +1,41 @@
 # csc_file.py
 # Encrypts and decrypts files using a Caesar Shift Cipher
-# 05/06/2017
+# 05/07/2017
 
 def get_file():
     fname = raw_input('  Enter a file name: ')
     return fname
+
+def crypt(f_plain, f_cipher, shift_val):
+    for line in f_plain:
+        ciphertext = ''
+        line = line.rstrip()
+        for char in line:
+            ciphertext+=(chr(ord(char) + shift_val))
+        print ciphertext #Debug
+        ciphertext = ciphertext + '\n'
+        f_cipher.write(ciphertext)
 
 # *******************************************************
 
 # Get plaintext filename
 print 'Choose the plaintext file to encrypt'
 plaintext_fname = get_file()
-#print plaintext_fname #Debug
-#exit()
 try:
     fhand_plain = open(plaintext_fname)
 except:
     print 'File cannot be opened:', plaintext_fname
     exit()
 
-
 # Get ciphertext filename
 print 'Choose the ciphertext file to create'
 ciphertext_fname = get_file()
-try:
-    fhand_cipher = open(ciphertext_fname)
-    print '  Warning: file already exists'
-    exit()
-    print '  End of try clause executing'
-except:
-    print 'Beginning of except clause executing' #Debug
-    fhand_cipher = open(ciphertext_fname, 'w')
-    print 'End of Except clause executing' #Debug
+fhand_cipher = open(ciphertext_fname, 'w')
 
+shift = int(raw_input('Enter a shift value: '))
+
+crypt(fhand_plain, fhand_cipher, shift)
+
+# Cleaning up
+fhand_plain.close()
 fhand_cipher.close()
-exit()
