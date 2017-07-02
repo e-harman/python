4f0c05984a28de64856f0f113fcd21eef326d6f6
e-harman <harman.eric@gmail.com>
2017-07-02 12:30:49 -0700

    initial commit

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,13 @@
+# FizzBuzz
+# From:
+#  https://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/
+
+for i in range(1,21):
+    if i % 3 == 0 and i % 5 == 0:
+        print 'FizzBuzz'
+    elif i % 3 == 0:
+        print 'Fizz'
+    elif i % 5 == 0:
+        print 'Buzz'
+    else:
+        print i
